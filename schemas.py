5a22ef66288900fd56fc257ed6255c7d43c23cec
Krishna Varathan <krishnavarathan@gmail.com>
2026-02-19 02:12:36 +0530

    Fourth Tutorial-Finished

--- schemas.py
+++ schemas.py
@@ -1,16 +1,39 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
 
-class Postbase(BaseModel):
-    title: str=Field(min_length=1, max_length=100)
-    content: str=Field(min_length=1)
-    author: str=Field(min_length=1, max_length=50)
+from datetime import datetime
 
 
-class PostCreate(Postbase):
+class UserBase(BaseModel):
+    username: str = Field(min_length=1, max_length=50)
+    email: EmailStr = Field(max_length=120)
+
+
+class UserCreate(UserBase):
     pass
 
-class PostResponse(Postbase):
+
+class UserResponse(UserBase):
+    # This class inhertes the UserBase, so usename & email will be public. Later we'll fix to public and private schemas 
+    model_config = ConfigDict(from_attributes=True)
+
+    id:int
+    image_file: str | None
+    image_path: str
+
+
+class PostBase(BaseModel):
+    title: str=Field(min_length=1, max_length=100)
+    content: str=Field(min_length=1)
+    
+
+
+class PostCreate(PostBase):
+    user_id: int # Temporary
+
+class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
-    date_posted: str
+    user_id: int
+    date_posted: datetime
+    author: UserResponse

a3b8e9f6e8c2204cde4c1af1b72b4f38d6b23221
Krishna Varathan <dexterkrish244@gmail.com>
2026-02-22 00:04:50 +0530

    FastAPI Tutorial (Part 11): Authorization - Protecting Routes and Verifying Current User

--- schemas.py
+++ schemas.py
@@ -40,7 +40,7 @@ class PostBase(BaseModel):
     
 
 class PostCreate(PostBase):
-    user_id: int # Temporary
+    pass
 
 class PostUpdate(BaseModel):
     title: str | None=Field(default=None, min_length=1, max_length=100)
